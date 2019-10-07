8496765a847d1f99b691800aaf48f9e6c0b7b7e3
ifelixking <felixking_powerbaes@hotmail.com>
2019-10-07 11:20:15 +0800

    MakeCurrent貌似可以并行

--- opengl/multiThread/main.cpp
+++ opengl/multiThread/main.cpp
@@ -1,3 +1,5 @@
+// 说明：demo for 主线程创建/删除资源，渲染线程使用资源
+// 两者貌似主代码层面上可以并行，因为将 MakeCurrent 中的锁注释掉也能正常运行
 
 #include <cassert>
 #include <string>
@@ -32,14 +34,15 @@ struct MakeCurrent{
     MakeCurrent(const Display * display, GLXDrawable win, GLXContext context)
             : m_display((Display *)display)
     {
-    	pthread_mutex_lock(&g_makecurrent_lock);
+    	// ？？问题：make current 是否是线程安全的
+    	// pthread_mutex_lock(&g_makecurrent_lock);
         m_oldContext = glXGetCurrentContext();
         m_oldWindow = glXGetCurrentDrawable();
         glXMakeCurrent(m_display, win, context);
     }
     ~MakeCurrent() {
         glXMakeCurrent(m_display, m_oldWindow, m_oldContext);
-		pthread_mutex_unlock(&g_makecurrent_lock);
+		// pthread_mutex_unlock(&g_makecurrent_lock);
     }
 };
 void openDevice(Device & device){
@@ -64,7 +67,7 @@ Window createWindow(const Device & device){
     swa.colormap = device.colormap;
     swa.event_mask = EVENT_MASK;
     unsigned long valueMask = CWColormap | CWEventMask;
-    auto window = XCreateWindow(device.display, device.windowRoot, 100, 100, 320, 240, 0,
+    auto window = XCreateWindow(device.display, device.windowRoot, 100, 100, 640, 480, 0,
                                 device.visualInfo->depth, InputOutput, device.visualInfo->visual,
                                 valueMask, &swa);
     XMapWindow(device.display, window);
@@ -98,32 +101,33 @@ GLXContext  g_context;
 Window g_window;
 bool g_exitFlag = false;
 
-const int DIM = 10;
-const float SIZE = 1.0f/DIM;
+const int DIM = 200;
+const float SIZE = 1.0f/DIM*2;
+const int MAX_LIFE_COUNT = 50;
 
 class Renderable{
 public:
-	Renderable(int position):
-		m_position(position){
+	Renderable(int position, int lifeCount):
+		m_position(position),
+		m_lifeCount(lifeCount){
 	}
 	// call in opengl resource thread
 	void initResource(){
-		float x = (float)(m_position % DIM) / DIM;
-		float y = (float)(m_position / DIM) / DIM;
+		float x = (float)(m_position % DIM) / DIM * 2 - 1.0f;
+		float y = (float)(m_position / DIM % DIM) / DIM * 2 - 1.0f;
 		float data[8] = {
 				x,y,
 				x-SIZE,y,
 				x-SIZE,y-SIZE,
 				x,y-SIZE,
 		};
-		MakeCurrent mc(g_device.display, g_device.windowRoot, g_context);
+
 		glGenBuffers(1, &m_vbo);
 		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
 		glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
 	}
 	// call in opengl resource thread
 	void freeResource(){
-		MakeCurrent mc(g_device.display, g_device.windowRoot, g_context);
 		glDeleteBuffers(1, &m_vbo);
 	}
 	// call in opengl render thread
@@ -132,24 +136,30 @@ public:
 		glEnableClientState(GL_VERTEX_ARRAY);
 		glVertexPointer(2, GL_FLOAT, sizeof(float)<<1, 0);
 		glDrawArrays(GL_QUADS, 0, 4);
+		--m_lifeCount;
+	}
+
+	bool needDelete() const{
+		return m_lifeCount <= 0;
 	}
 
 private:
 	int m_position;
 	GLuint m_vbo;
+	mutable int m_lifeCount;
 };
 
 // ===========================================================================================
 
 
 pthread_mutex_t g_newRenderables_lock;
-std::list<Renderable> g_newRenderables;
+std::list<Renderable *> g_newRenderables;
 pthread_mutex_t g_deleteRenderables_lock;
-std::list<Renderable> g_deleteRenderables;
+std::list<Renderable *> g_deleteRenderables;
 
 void eventLoopWithRender(Display * display, GLXContext context){
     XEvent event;
-    std::vector<Renderable> tmp;
+    std::vector<Renderable *> tmp;
     for (;;){
 		if (XCheckMaskEvent(display, EVENT_MASK, &event)){
 			switch (event.type){
@@ -163,18 +173,24 @@ void eventLoopWithRender(Display * display, GLXContext context){
 			}
 		}else{
 			MakeCurrent mc(display, g_window, context);
-
 			glClearColor(1.0f, 0.5f, 0.5f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			pthread_mutex_lock(&g_newRenderables_lock);
 			tmp.assign(g_newRenderables.begin(), g_newRenderables.end());
-			g_newRenderables.clear();
 			pthread_mutex_unlock(&g_newRenderables_lock);
 
-			for(auto & item : tmp){
-				item.render();
+			for(auto & item : tmp) { item->render(); }
+
+			tmp.clear();
+			pthread_mutex_lock(&g_newRenderables_lock);
+			for(auto itor=g_newRenderables.begin(); itor!=g_newRenderables.end(); ++itor){
+				if ((*itor)->needDelete()){
+					tmp.push_back(*itor);
+					itor = g_newRenderables.erase(itor);
+				}
 			}
+			pthread_mutex_unlock(&g_newRenderables_lock);
 
 			pthread_mutex_lock(&g_deleteRenderables_lock);
 			g_deleteRenderables.insert(g_deleteRenderables.end(), tmp.begin(), tmp.end());
@@ -206,48 +222,32 @@ int main(){
     initGLEW(g_device, g_context);
     g_window = createWindow(g_device);
 
+    pthread_t thread; iResult = pthread_create(&thread, nullptr, threadRender, nullptr); assert(iResult == 0);
 
-
-
-//	{
-//		Renderable renderable(1);
-//		renderable.initResource();
-//
-//		pthread_mutex_lock(&g_newRenderables_lock);
-//		g_newRenderables.push_back(renderable);
-//		pthread_mutex_unlock(&g_newRenderables_lock);
-//
-//		// pthread_mutex_lock(&g_deleteRenderables_lock);
-//		// tmp.assign(g_deleteRenderables.begin(), g_deleteRenderables.end());
-//		// g_deleteRenderables.clear();
-//		// pthread_mutex_unlock(&g_deleteRenderables_lock);
-//	}
-
-    pthread_t thread;
-    iResult = pthread_create(&thread, nullptr, threadRender, nullptr); assert(iResult == 0);
-
-    std::vector<Renderable> tmp;
+    std::vector<Renderable *> tmp;
 
     for (int i=0;!g_exitFlag;++i){
 
+		auto renderable = new Renderable (i, MAX_LIFE_COUNT);
+		{
+			MakeCurrent mc(g_device.display, g_device.windowRoot, g_context);
+			renderable->initResource();
+		}
 
-    	//if (i < 100) {
-			Renderable renderable(i);
-			renderable.initResource();
+		pthread_mutex_lock(&g_newRenderables_lock);
+		g_newRenderables.push_back(renderable);
+		pthread_mutex_unlock(&g_newRenderables_lock);
 
-			pthread_mutex_lock(&g_newRenderables_lock);
-			g_newRenderables.push_back(renderable);
-			pthread_mutex_unlock(&g_newRenderables_lock);
+		tmp.clear();
+		pthread_mutex_lock(&g_deleteRenderables_lock);
+		tmp.assign(g_deleteRenderables.begin(), g_deleteRenderables.end());
+		g_deleteRenderables.clear();
+		pthread_mutex_unlock(&g_deleteRenderables_lock);
 
-			pthread_mutex_lock(&g_deleteRenderables_lock);
-			tmp.assign(g_deleteRenderables.begin(), g_deleteRenderables.end());
-			g_deleteRenderables.clear();
-			pthread_mutex_unlock(&g_deleteRenderables_lock);
-		// }
-			for (auto &item : tmp) {
-				item.freeResource();
-			}
-
+		{
+			MakeCurrent mc(g_device.display, g_device.windowRoot, g_context);
+			for (auto &item : tmp) { item->freeResource(); }
+		}
 
         pthread_yield();
     }
